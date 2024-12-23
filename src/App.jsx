24c1842f806a5b454470e03aff667a3f2043e6ac
GGGMatt89 <matt.fntn89@gmail.com>
2024-12-23 09:35:12 +0100

    App completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, //force conversion to number if a string is received (+newValue)
+      };
+    });
+  }
+
+  const inputIsValid = userInput.duration >= 1;
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater then 0.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
